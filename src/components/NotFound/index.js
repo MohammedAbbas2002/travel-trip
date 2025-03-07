2e1bc30c956a478457795a18b35a3f9973b5c1cd
MohammedAbbas2002 <mohammedabbas91760@gmail.com>
2025-03-07 13:39:44 +0000

    Travel Trip

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,5 +1,9 @@
 import './index.css'
 
-const NotFound = () => {}
+const NotFound = () => (
+  <div className="mobile-not-found-container">
+    <img src="https://res.cloudinary.com/dt7mi4nem/image/upload/v1741308981/Group_7520_x94tnd.png" />
+  </div>
+)
 
 export default NotFound
