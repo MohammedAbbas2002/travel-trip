96c9d9951db57186ead2514cfddfb3ac70a2cac1
MohammedAbbas2002 <mohammedabbas91760@gmail.com>
2025-03-07 14:06:39 +0000

    Travel Trip

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,16 +1,28 @@
 import './index.css'
 
 const NotFound = () => (
-  <div className="mobile-not-found-container">
-    <img
-      src="https://res.cloudinary.com/dt7mi4nem/image/upload/v1741308981/Group_7520_x94tnd.png"
-      className="not-found-image"
-    />
-    <h1 className="not-found-heading">Page Not Found.</h1>
-    <p className="not-found-description">
-      We are sorry, the page you requested could not be found.
-    </p>
-  </div>
+  <>
+    <div className="mobile-not-found-container">
+      <img
+        src="https://res.cloudinary.com/dt7mi4nem/image/upload/v1741308981/Group_7520_x94tnd.png"
+        className="mobile-not-found-image"
+      />
+      <h1 className="mobile-not-found-heading">Page Not Found.</h1>
+      <p className="mobile-not-found-description">
+        We are sorry, the page you requested could not be found.
+      </p>
+    </div>
+    <div className="desktop-not-found-container">
+      <img
+        src="https://res.cloudinary.com/dt7mi4nem/image/upload/v1741308981/Group_7520_x94tnd.png"
+        className="desktop-not-found-image"
+      />
+      <h1 className="desktop-not-found-heading">Page Not Found.</h1>
+      <p className="desktop-not-found-description">
+        We are sorry, the page you requested could not be found.
+      </p>
+    </div>
+  </>
 )
 
 export default NotFound
