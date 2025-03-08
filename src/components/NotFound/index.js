f648c46824759a2e2d31334351f365b420cf72d2
MohammedAbbas2002 <mohammedabbas91760@gmail.com>
2025-03-08 10:08:10 +0000

    Travel Trip

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -2,25 +2,14 @@ import './index.css'
 
 const NotFound = () => (
   <>
-    <div className="mobile-not-found-container">
+    <div className="not-found-container">
       <img
         src="https://res.cloudinary.com/dt7mi4nem/image/upload/v1741308981/Group_7520_x94tnd.png"
-        className="mobile-not-found-image"
+        className="not-found-image"
         alt="not found"
       />
-      <h1 className="mobile-not-found-heading">Page Not Found.</h1>
-      <p className="mobile-not-found-description">
-        We are sorry, the page you requested could not be found.
-      </p>
-    </div>
-    <div className="desktop-not-found-container">
-      <img
-        src="https://res.cloudinary.com/dt7mi4nem/image/upload/v1741308981/Group_7520_x94tnd.png"
-        className="desktop-not-found-image"
-        alt="not found"
-      />
-      <h1 className="desktop-not-found-heading">Page Not Found.</h1>
-      <p className="desktop-not-found-description">
+      <h1 className="not-found-heading">Page Not Found.</h1>
+      <p className="not-found-description">
         We are sorry, the page you requested could not be found.
       </p>
     </div>
