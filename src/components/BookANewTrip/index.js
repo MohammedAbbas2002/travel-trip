eeadf29c145ed3876875072fa6ac7858b13be6cb
MohammedAbbas2002 <mohammedabbas91760@gmail.com>
2025-03-08 14:18:54 +0000

    Travel Trip

--- src/components/BookANewTrip/index.js
+++ src/components/BookANewTrip/index.js
@@ -125,179 +125,93 @@ class BookANewTrip extends Component {
       : ''
 
     return (
-      <>
-        <div className="mobile-your-details-container">
-          <h1 className="mobile-your-details-heading">Your Details</h1>
-          <p className="mobile-your-details-description">
-            Enter your name and location details
-          </p>
-          <form onSubmit={this.onSubmitYourDetailsForm}>
-            <div className="name-input-container">
-              <label htmlFor="name" className="label">
-                Name
-              </label>
-              <br />
-              <div
-                className={`name-input-and-error-icon-container ${nameRedBorder}`}
-              >
-                <input
-                  type="text"
-                  placeholder="Enter name"
-                  className={`name-input ${nameRedPlaceholderText}`}
-                  id="name"
-                  value={name}
-                  onChange={this.onChangeName}
-                />
-                {isNameEmpty && <MdErrorOutline size="20" color="#ef4444" />}
-              </div>
+      <div className="your-details-container">
+        <h1 className="your-details-heading">Your Details</h1>
+        <p className="your-details-description">
+          Enter your name and location details
+        </p>
+        <form
+          className="your-details-form"
+          onSubmit={this.onSubmitYourDetailsForm}
+        >
+          <div className="name-input-container">
+            <label htmlFor="name" className="label">
+              Name
+            </label>
+            <br />
+            <div
+              className={`name-input-and-error-icon-container ${nameRedBorder}`}
+            >
+              <input
+                type="text"
+                placeholder="Enter name"
+                className={`name-input ${nameRedPlaceholderText}`}
+                id="name"
+                value={name}
+                onChange={this.onChangeName}
+              />
+              {isNameEmpty && <MdErrorOutline size="20" color="#ef4444" />}
             </div>
-            {isNameEmpty && <p className="error-message">Enter your name</p>}
+          </div>
+          {isNameEmpty && <p className="error-message">Enter your name</p>}
 
-            <div className="start-location-input-container">
-              <label htmlFor="startLocation" className="label">
-                Start location
-              </label>
-              <br />
-              <div
-                className={`start-location-input-and-error-icon-container ${startLocationRedBorder}`}
-              >
-                <input
-                  type="text"
-                  placeholder="Enter name"
-                  className={`start-location-input ${startLocationRedPlaceholderText}`}
-                  id="startLocation"
-                  value={startLocation}
-                  onChange={this.onChangeStartLocation}
-                />
-                {isStartLocationEmpty && (
-                  <MdErrorOutline size="20" color="#ef4444" />
-                )}
-              </div>
+          <div className="start-location-input-container">
+            <label htmlFor="startLocation" className="label">
+              Start location
+            </label>
+            <br />
+            <div
+              className={`start-location-input-and-error-icon-container ${startLocationRedBorder}`}
+            >
+              <input
+                type="text"
+                placeholder="Enter name"
+                className={`start-location-input ${startLocationRedPlaceholderText}`}
+                id="startLocation"
+                value={startLocation}
+                onChange={this.onChangeStartLocation}
+              />
+              {isStartLocationEmpty && (
+                <MdErrorOutline size="20" color="#ef4444" />
+              )}
             </div>
-            {isStartLocationEmpty && (
-              <p className="error-message">Enter your start location</p>
-            )}
+          </div>
+          {isStartLocationEmpty && (
+            <p className="error-message">Enter your start location</p>
+          )}
 
-            <div className="end-location-input-container">
-              <label htmlFor="endLocation" className="label">
-                End location
-              </label>
-              <br />
-              <div
-                className={`end-location-input-and-error-icon-container ${endLocationRedBorder}`}
-              >
-                <input
-                  type="text"
-                  placeholder="Enter end location"
-                  className={`end-location-input ${endLocationRedPlaceholderText}`}
-                  id="endLocation"
-                  value={endLocation}
-                  onChange={this.onChangeEndLocation}
-                />
-                {isEndLocationEmpty && (
-                  <MdErrorOutline size="20" color="#ef4444" />
-                )}
-              </div>
+          <div className="end-location-input-container">
+            <label htmlFor="endLocation" className="label">
+              End location
+            </label>
+            <br />
+            <div
+              className={`end-location-input-and-error-icon-container ${endLocationRedBorder}`}
+            >
+              <input
+                type="text"
+                placeholder="Enter end location"
+                className={`end-location-input ${endLocationRedPlaceholderText}`}
+                id="endLocation"
+                value={endLocation}
+                onChange={this.onChangeEndLocation}
+              />
+              {isEndLocationEmpty && (
+                <MdErrorOutline size="20" color="#ef4444" />
+              )}
             </div>
-            {isEndLocationEmpty && (
-              <p className="error-message">Enter your end location</p>
-            )}
+          </div>
+          {isEndLocationEmpty && (
+            <p className="error-message">Enter your end location</p>
+          )}
 
-            <div className="next-button-container">
-              <button type="submit" className="next-button">
-                Next
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="desktop-your-details-container">
-          <h1 className="desktop-your-details-heading">Your Details</h1>
-          <p className="desktop-your-details-description">
-            Enter your name and location details
-          </p>
-          <form
-            className="desktop-your-details-form"
-            onSubmit={this.onSubmitYourDetailsForm}
-          >
-            <div className="name-input-container">
-              <label htmlFor="name" className="label">
-                Name
-              </label>
-              <br />
-              <div
-                className={`name-input-and-error-icon-container ${nameRedBorder}`}
-              >
-                <input
-                  type="text"
-                  placeholder="Enter name"
-                  className={`name-input ${nameRedPlaceholderText}`}
-                  id="name"
-                  value={name}
-                  onChange={this.onChangeName}
-                />
-                {isNameEmpty && <MdErrorOutline size="20" color="#ef4444" />}
-              </div>
-            </div>
-            {isNameEmpty && <p className="error-message">Enter your name</p>}
-
-            <div className="start-location-input-container">
-              <label htmlFor="startLocation" className="label">
-                Start location
-              </label>
-              <br />
-              <div
-                className={`start-location-input-and-error-icon-container ${startLocationRedBorder}`}
-              >
-                <input
-                  type="text"
-                  placeholder="Enter name"
-                  className={`start-location-input ${startLocationRedPlaceholderText}`}
-                  id="startLocation"
-                  value={startLocation}
-                  onChange={this.onChangeStartLocation}
-                />
-                {isStartLocationEmpty && (
-                  <MdErrorOutline size="20" color="#ef4444" />
-                )}
-              </div>
-            </div>
-            {isStartLocationEmpty && (
-              <p className="error-message">Enter your start location</p>
-            )}
-
-            <div className="end-location-input-container">
-              <label htmlFor="endLocation" className="label">
-                End location
-              </label>
-              <br />
-              <div
-                className={`end-location-input-and-error-icon-container ${endLocationRedBorder}`}
-              >
-                <input
-                  type="text"
-                  placeholder="Enter end location"
-                  className={`end-location-input ${endLocationRedPlaceholderText}`}
-                  id="endLocation"
-                  value={endLocation}
-                  onChange={this.onChangeEndLocation}
-                />
-                {isEndLocationEmpty && (
-                  <MdErrorOutline size="20" color="#ef4444" />
-                )}
-              </div>
-            </div>
-            {isEndLocationEmpty && (
-              <p className="error-message">Enter your end location</p>
-            )}
-
-            <div className="desktop-next-button-container">
-              <button type="submit" className="next-button">
-                Next
-              </button>
-            </div>
-          </form>
-        </div>
-      </>
+          <div className="next-button-container">
+            <button type="submit" className="next-button">
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
     )
   }
 
@@ -373,121 +287,64 @@ class BookANewTrip extends Component {
       : ''
 
     return (
-      <>
-        <div className="mobile-date-selection-container">
-          <h1 className="mobile-date-selection-heading">Date Selection</h1>
-          <p className="mobile-date-selection-description">
-            Select your Start and End Date.
-          </p>
-          <form onSubmit={this.onSubmitDateSelectionForm}>
-            <div className="start-date-input-container">
-              <label htmlFor="startDate" className="label">
-                Start Date
-              </label>
-              <br />
-              <input
-                type="date"
-                className={`start-date-input ${startDateRedBorder} ${startDateRedPlaceholderText}`}
-                id="startDate"
-                value={startDate}
-                onChange={this.onChangeStartDate}
-              />
-            </div>
-            {isStartDateEmpty && (
-              <p className="error-message">Select start date</p>
-            )}
-            <div className="end-date-input-container">
-              <label htmlFor="endDate" className="label">
-                End Date
-              </label>
-              <br />
-              <input
-                type="date"
-                className={`end-date-input ${endDateRedBorder} ${endDateRedPlaceholderText}`}
-                id="endDate"
-                value={endDate}
-                onChange={this.onChangeEndDate}
-              />
-            </div>
-            {isEndDateEmpty && <p className="error-message">Select end date</p>}
-            {isEndDateLesserThanStartDate && (
-              <p className="error-message">
-                The end date cannot be less than the start date
-              </p>
-            )}
-            <div className="previous-and-next-button-container">
-              <button
-                type="button"
-                className="previous-button"
-                onClick={this.onGoToYourDetailsForm}
-              >
-                Previous
-              </button>
-              <button type="submit" className="next-button">
-                Next
-              </button>
-            </div>
-          </form>
-        </div>
-        <div className="desktop-date-selection-container">
-          <h1 className="desktop-date-selection-heading">Date Selection</h1>
-          <p className="desktop-date-selection-description">
-            Select your Start and End Date.
-          </p>
-          <form
-            className="desktop-date-selection-form"
-            onSubmit={this.onSubmitDateSelectionForm}
-          >
-            <div className="start-date-input-container">
-              <label htmlFor="startDate" className="label">
-                Start Date
-              </label>
-              <br />
-              <input
-                type="date"
-                className={`start-date-input ${startDateRedBorder} ${startDateRedPlaceholderText}`}
-                id="startDate"
-                value={startDate}
-                onChange={this.onChangeStartDate}
-              />
-            </div>
-            {isStartDateEmpty && (
-              <p className="error-message">Select start date</p>
-            )}
-            <div className="end-date-input-container">
-              <label htmlFor="endDate" className="label">
-                End Date
-              </label>
-              <br />
-              <input
-                type="date"
-                className={`end-date-input ${endDateRedBorder} ${endDateRedPlaceholderText}`}
-                id="endDate"
-                value={endDate}
-                onChange={this.onChangeEndDate}
-              />
-            </div>
-            {isEndDateEmpty && <p className="error-message">Select end date</p>}
-            {isEndDateLesserThanStartDate && (
-              <p className="error-message">
-                The end date cannot be less than the start date
-              </p>
-            )}
-            <div className="desktop-previous-and-next-button-container">
-              <button
-                type="button"
-                className="previous-button"
-                onClick={this.onGoToYourDetailsForm}
-              >
-                Previous
-              </button>
-              <button type="submit" className="next-button">
-                Next
-              </button>
-            </div>
-          </form>
-        </div>
-      </>
+      <div className="date-selection-container">
+        <h1 className="date-selection-heading">Date Selection</h1>
+        <p className="date-selection-description">
+          Select your Start and End Date.
+        </p>
+        <form
+          className="date-selection-form"
+          onSubmit={this.onSubmitDateSelectionForm}
+        >
+          <div className="start-date-input-container">
+            <label htmlFor="startDate" className="label">
+              Start Date
+            </label>
+            <br />
+            <input
+              type="date"
+              className={`start-date-input ${startDateRedBorder} ${startDateRedPlaceholderText}`}
+              id="startDate"
+              value={startDate}
+              onChange={this.onChangeStartDate}
+            />
+          </div>
+          {isStartDateEmpty && (
+            <p className="error-message">Select start date</p>
+          )}
+          <div className="end-date-input-container">
+            <label htmlFor="endDate" className="label">
+              End Date
+            </label>
+            <br />
+            <input
+              type="date"
+              className={`end-date-input ${endDateRedBorder} ${endDateRedPlaceholderText}`}
+              id="endDate"
+              value={endDate}
+              onChange={this.onChangeEndDate}
+            />
+          </div>
+          {isEndDateEmpty && <p className="error-message">Select end date</p>}
+          {isEndDateLesserThanStartDate && (
+            <p className="error-message">
+              The end date cannot be less than the start date
+            </p>
+          )}
+          <div className="previous-and-next-button-container">
+            <button
+              type="button"
+              className="previous-button"
+              onClick={this.onGoToYourDetailsForm}
+            >
+              Previous
+            </button>
+            <button type="submit" className="next-button">
+              Next
+            </button>
+          </div>
+        </form>
+      </div>
     )
   }
 
