d54d32523e94fead9b52653ef3c245b17a471240
MohammedAbbas2002 <mohammedabbas91760@gmail.com>
2025-03-08 06:58:53 +0000

    Travel Trip

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,48 +7,30 @@ import './index.css'
 
 const Home = () => (
   <>
-    {/* Mobile Device */}
-    <div className="mobile-home-container">
-      <div className="mobile-home-image-container">
+    <Header activeNavbarItem="HOME" />
+    <div className="home-container">
+      <div className="home-image-container">
         <img
           src="https://res.cloudinary.com/dt7mi4nem/image/upload/v1741060875/image_5_1_hvzpl8.png"
-          className="mobile-home-image"
+          className="home-image"
         />
       </div>
-      <h1 className="mobile-home-heading">Travel. Relax. Memories.</h1>
-      <p className="mobile-home-description">
-        With travel trip you can experience new travel and the best tourist
-        destinations.
-      </p>
-      <div className="mobile-home-book-a-new-trip-button-container">
-        <Link to="/book-a-new-trip" className="link">
-          <button type="button" className="mobile-home-book-a-new-trip-button">
-            Book a New Trip
-          </button>
-        </Link>
-      </div>
-    </div>
-    <Footer activeNavbarItem="HOME" />
-    {/* Desktop Device */}
-    <Header activeNavbarItem="HOME" />
-    <div className="desktop-home-container">
-      <div className="desktop-home-content-container">
-        <h1 className="desktop-home-heading">Travel. Relax. Memories.</h1>
-        <p className="desktop-home-description">
+      <div className="home-content-container">
+        <h1 className="home-heading">Travel. Relax. Memories.</h1>
+        <p className="home-description">
           With travel trip you can experience new travel and the best tourist
           destinations.
         </p>
-        <Link to="/book-a-new-trip" className="link">
-          <button type="button" className="desktop-home-book-a-new-trip-button">
-            Book a New Trip
-          </button>
-        </Link>
+        <div className="home-book-a-new-trip-button-container">
+          <Link to="/book-a-new-trip" className="link">
+            <button type="button" className="home-book-a-new-trip-button">
+              Book a New Trip
+            </button>
+          </Link>
+        </div>
       </div>
-      <img
-        src="https://res.cloudinary.com/dt7mi4nem/image/upload/v1741060456/image_5_zvyxp6.png"
-        className="desktop-home-image"
-      />
     </div>
+    <Footer activeNavbarItem="HOME" />
   </>
 )
 
